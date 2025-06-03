ee4f913ddcdd9234c5e5f65da251dc27174fdb5e
Mohammad Pourmami <m.pourmami@gmail.com>
2025-06-04 00:39:21 +0330

    feat (Auth): Add rate limiter to routes

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -6,12 +6,12 @@ use Modules\Auth\app\Http\Controllers\ForgetPasswordController;
 
 Route::prefix('auth')->group(function () {
 
-    Route::post('send-code',    [AuthController::class, 'sendActivationCode']);
+    Route::post('send-code',    [AuthController::class, 'sendActivationCode'])->middleware('throttle:3,2');;
     Route::post('verify-code',  [AuthController::class, 'verifyActivationCode']);
 
     Route::middleware(['auth:sanctum', 'ability:complete-register'])
         ->post('complete-register', [AuthController::class, 'completeRegister']);
 
-    Route::post('/forgot-password', [ForgetPasswordController::class, 'requestReset']);
+    Route::post('/forgot-password', [ForgetPasswordController::class, 'requestReset'])->middleware('throttle:3,2');;
     Route::post('/reset-password',  [ForgetPasswordController::class, 'resetPassword']);
 });
